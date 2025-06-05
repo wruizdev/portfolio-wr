43af87964fd2c812e78f58233f1883da1f173a63
wruizdev <wruizdev@gmail.com>
2025-06-05 11:08:21 +0200

    Añadir ficheros footer.html y footer.js y unir a index.html

---
+++ js/footer.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("./footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footerContainer").innerHTML = data;
+        })
+        .catch(error => console.error("Error al cargar el footer:", error));
+});
